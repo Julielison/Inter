d8989aae3dac70375cd2ad392558da0c68b0539f
Igor André <imigorgm@gmail.com>
2024-04-22 19:30:36 -0300

    Teste

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_name_user():
     # Instancia uma conexão com o banco de dados
     data = Database()
 
-    # Checa se o nome de usuário já existe
+    # Checa se o nome de usuário já existe 
     #if data.check_username(username):
     #     pass
 
